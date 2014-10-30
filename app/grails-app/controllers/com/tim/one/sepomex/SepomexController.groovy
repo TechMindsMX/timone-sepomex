9f83a1eba309647bca1a49a5736c0e0e59880293
josdem <joseluis.delacruz@gmail.com>
2014-10-30 15:29:10 -0600

    Agregando sepomex formato en un solo bean con lista de asentamientos

--- app/grails-app/controllers/com/tim/one/sepomex/SepomexController.groovy
+++ app/grails-app/controllers/com/tim/one/sepomex/SepomexController.groovy
@@ -4,9 +4,30 @@ class SepomexController {
 
 	static allowedMethods = [show:'GET']
 
-    def show(String id){
+	def show(String id) {
+    def sepomexes = Sepomex.findAllByDCodigo(id)
+
+    def jsonData = [
+      id:sepomexes[0]?.id,
+      dCodigo:sepomexes[0]?.dCodigo,
+      dTipoAsenta:sepomexes[0]?.dTipoAsenta,
+      dMnpio:sepomexes[0]?.dMnpio,
+      dEstado:sepomexes[0]?.dEstado,
+      dCiudad:sepomexes[0]?.dCiudad,
+      dCP:sepomexes[0]?.dCP,
+      cEstado:sepomexes[0]?.cEstado,
+      cOficina:sepomexes[0]?.cOficina,
+      cCP:sepomexes[0]?.cCP,
+      cTipoAsenta:sepomexes[0]?.cTipoAsenta,
+      cMnipio:sepomexes[0]?.cMnipio,
+      idAsentaCpcnos:sepomexes[0]?.idAsentaCpcnos,
+      dZona:sepomexes[0]?.dZona,
+      cCveCiudad:sepomexes[0]?.cCveCiudad,
+      dAsenta: sepomexes*.dAsenta
+    ]
+
     render(contentType:"application/json", status:200) {
-      Sepomex.findByDCodigo(id)
+      jsonData
     }
   }
   
