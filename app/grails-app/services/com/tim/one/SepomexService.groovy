868c9a8d7f22722df08a1c89037a006f3be29a4e
josdem <joseluis.delacruz@gmail.com>
2014-10-30 15:01:11 -0600

    Agregando sepomex service

---
+++ app/grails-app/services/com/tim/one/SepomexService.groovy
@@ -0,0 +1,11 @@
+package com.tim.one
+
+import grails.transaction.Transactional
+
+@Transactional
+class SepomexService {
+
+    def serviceMethod() {
+
+    }
+}
---
+++ app/test/unit/com/tim/one/SepomexServiceSpec.groovy
@@ -0,0 +1,20 @@
+package com.tim.one
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(SepomexService)
+class SepomexServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
